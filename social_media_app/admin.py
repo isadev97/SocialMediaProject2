23efbc83a9143db02a41e8078daf86b65d78ccf5
isadev97 <isa325697@gmail.com>
2023-06-24 21:48:19 +0530

    basic navbar sign up sign in page added

--- social_media_app/admin.py
+++ social_media_app/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from social_media_app.models import User, Profile, Post, LikePost
+from social_media_app.models import Profile, Post, LikePost
 
 # Register your models here.
-admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(LikePost)
